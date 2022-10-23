510d9f507b128132cef7a037e79bef884c23e027
sykwon2021 <sykwon@umass.edu>
2022-10-23 23:08:23 +0000

    hw2

---
+++ readfileCopy_RM.py
@@ -0,0 +1,35 @@
+file1 = open('query.titles_RM1_Q1_FINAL.json','w')
+queries= open("query.titles.tsv",'r')
+
+a = '{'
+b = '\n\t"requested": 1000,'
+c = '\n\t"index": "/home/student/robust04-complete-index",'
+d = '\n\t"mu": 1000,'
+e = '\n\t"scorer" : "dirichlet",'
+f = '\n\t"queries": ['
+g = '\n\t]'
+h = '\n}'
+
+file1.write(a+b+c+d+e+f)
+Lines = queries.readlines()
+linecount = 0
+for line in Lines:
+    sentence = line.split()
+    formattedSentence = ['\n\t\t{','\n\t\t"number": ']
+    count = 0
+    for i in sentence:
+        if count == 0:
+            formattedSentence.append('"' + i + '",')
+        elif count == 1:
+            formattedSentence.append('\n\t\t"text": "#rm(' + i)
+        else:
+            formattedSentence.append(' '+ i)
+        count += 1
+    formattedSentence.append(')"\n\t\t}')
+    linecount += 1
+    if linecount < len(Lines):
+        formattedSentence.append(',')
+    file1.write(''.join(formattedSentence))
+file1.writelines(g+h)
+file1.close()
+queries.close()
